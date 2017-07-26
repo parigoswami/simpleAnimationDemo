62d768a07dea6c4061c76af80427f9d582b3fbb5
parigoswami <goswamiutangpari@yahoo.in>
2017-07-26 20:31:01 +0530

    I'm able to pass array from one View controller to another View Controller.

--- animationOne/ViewController.swift
+++ animationOne/ViewController.swift
@@ -12,20 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet var viewObject: UIView!
     
-    @IBOutlet var secondView: UIView!
     @IBOutlet weak var visualEffectView: UIVisualEffectView!
     
-    @IBOutlet weak var secondTextView: UITextView!
     @IBOutlet weak var textField: UITextField!
     
     var returnedText : String = " "
     var effect = UIVisualEffect()
     
+    var myArr=[String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         effect=visualEffectView.effect!
         visualEffectView.effect=nil
-        secondView.layer.cornerRadius=8
         viewObject.layer.cornerRadius=5
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -63,30 +63,16 @@ class ViewController: UIViewController {
         returnedText=textField.text!
         animateOut()
         print("_______________rturnedText is : ",(returnedText))
-        self.view.addSubview(secondView)
-        secondView.transform=CGAffineTransform.init(scaleX: 1.3, y: 1.3)
-        secondView.center=self.view.center
-        secondView.alpha=0
-        UIView.animate(withDuration: 0.3, animations: {
-            self.secondView.alpha=1
-            self.secondView.transform=CGAffineTransform.identity
-            
-        })
-        secondTextView.text=returnedText
+        myArr.append(returnedText)
+        print("______myArr is ",myArr)
+        
     }
     
-    @IBAction func secondDonebutton(_ sender:Any){
-        
-        UIView.animate(withDuration: 0.3, animations: {
-            self.secondView.transform=CGAffineTransform.init(scaleX: 1.3, y: 1.3)
-            self.secondView.alpha=0
-            self.visualEffectView.effect=nil
-            
-        }){(sucess:Bool)in
-            self.secondView.removeFromSuperview()
-        }
-        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondvc : myTableViewController = segue.destination as! myTableViewController
+        secondvc.tempArr = myArr
     }
+    
     
     
     override func didReceiveMemoryWarning() {
